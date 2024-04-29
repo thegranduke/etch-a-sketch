1883e6001b26bb435fc2a250dde74b37f1007f05
Eino <einompi@gmail.com>
2024-04-30 01:23:33 +0200

    Create the colorShading function and enable the Rainbow and Shading functions to toggle their effects

--- script.js
+++ script.js
@@ -18,8 +18,9 @@ function createGrid(GRIDSIZE){
             gridDiv.style.height = divDimensions + "px";
             gridDiv.style.border = "1px solid black";
             gridDiv.style.boxSizing = "border-box";
-            gridDiv.classList.add("gridDivs")
-            gridDiv.addEventListener("mouseenter", randomColor);
+            gridDiv.classList.add("gridDivs");
+            gridDiv.style.backgroundColor = "rgb(255,255,255)";
+            gridDiv.addEventListener("mouseenter", solidColor);
             container.appendChild(gridDiv);
         }
     }
@@ -39,12 +40,35 @@ function randomColor(){
     let green = Math.floor(Math.random() * 256);
     let blue = Math.floor(Math.random() * 256);
 
-    let redHex = red.toString(16);
-    let greenHex = green.toString(16);
-    let blueHex = blue.toString(16);
+    let redHex = red.toString(16).padStart(2, '0');
+    let greenHex = green.toString(16).padStart(2, '0');
+    let blueHex = blue.toString(16).padStart(2, '0');
 
     color = "#" + redHex + greenHex + blueHex;
     this.style.backgroundColor = color;
+}
+
+function colorShading(color,element){
+    console.log(color);
+    
+    let formattedValue = color.match(/\d+/g).map(Number);
+    
+    let red = Math.max(formattedValue[0] - 25, 0);
+    let green = Math.max(formattedValue[1] - 25, 0);
+    let blue = Math.max(formattedValue[2] - 25, 0);
+
+
+    console.log(formattedValue);
+    console.log(red);
+    console.log(green);
+    console.log(blue);
+
+    let newColor = "rgb(" + red + "," + green + "," + blue + ")";
+
+    element.style.backgroundColor = newColor;
+    
+    
+    return newColor;
 }
 
 
@@ -58,6 +82,33 @@ document.addEventListener("DOMContentLoaded", ()=> {
     slider.addEventListener("input", ()=> {
         let gridSize = slider.value;
         createGrid(gridSize);      
+    });
+
+    const rainbowButton = document.querySelector(".rainbow-button");
+    rainbowButton.addEventListener("click", () =>{
+        let gridDivs = document.querySelectorAll(".gridDivs");
+        for( let i = 0; i < gridDivs.length; i++){
+            gridDivs[i].addEventListener("mouseenter",randomColor);
+        }
+
+    });
+
+    const shadingButton = document.querySelector(".shading-button");
+    shadingButton.addEventListener("click", () =>{
+        let gridDivs = document.querySelectorAll(".gridDivs");
+        for( let i = 0; i < gridDivs.length; i++){
+
+            // Store the original color of each gridDiv using a custom data attribute
+            gridDivs[i].setAttribute('data-original-color', gridDivs[i].style.backgroundColor);
+            
+            gridDivs[i].addEventListener("mouseenter",(event) => { 
+
+                let divColor = event.target.getAttribute('data-original-color');
+                divColor = colorShading(divColor,event.target);
+                gridDivs[i].setAttribute('data-original-color', divColor);
+            });
+        }
+
     });
 
     
